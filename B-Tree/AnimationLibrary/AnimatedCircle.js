f9852a2f2f7fa62105c45f8a91435fe354ab60f4
jllanosr452 <85802210+jllanosr452@users.noreply.github.com>
2021-07-02 09:35:11 -0500

    actualizacion de animated treenode circle y label

--- B-Tree/AnimationLibrary/AnimatedCircle.js
+++ B-Tree/AnimationLibrary/AnimatedCircle.js
@@ -1,28 +1,29 @@
 // Copyright 2011 David Galles, University of San Francisco. All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without modification, are
-// permitted provided that the following conditions are met:
+// Se permite la redistribución y el uso en formato fuente y binario, con o sin 
+// modificaciones, siempre que se cumplan las siguientes condiciones:
 //
-// 1. Redistributions of source code must retain the above copyright notice, this list of
-// conditions and the following disclaimer.
+// 1. Las redistribuciones del código fuente deben conservar el aviso de derechos de 
+// autor anterior, esta lista de condiciones y el siguiente descargo de responsabilidad.
 //
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list
-// of conditions and the following disclaimer in the documentation and/or other materials
-// provided with the distribution.
+// 2. Las redistribuciones en formato binario deben reproducir el aviso de derechos de autor
+//  anterior, esta lista de condiciones y el siguiente descargo de responsabilidad en la
+// documentación y / u otros materiales proporcionados con la distribución.
 //
-// THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ESTE SOFTWARE ES PROPORCIONADO POR David Galles `` TAL CUAL '' Y CUALQUIER GARANTÍA EXPLÍCITA
+// O IMPLÍCITA, INCLUYENDO, PERO NO LIMITADO A, LAS GARANTÍAS IMPLÍCITAS DE COMERCIABILIDAD
+// E IDONEIDAD PARA UN PROPÓSITO PARTICULAR NO SE RESPONSABILIZA. EN NINGÚN CASO David Galles O LOS
+// COLABORADORES SERÁN RESPONSABLES DE NINGÚN DAÑO DIRECTO, INDIRECTO, INCIDENTAL, ESPECIAL, EJEMPLAR
+// O CONSECUENCIAL (INCLUYENDO, PERO NO LIMITADO A, LA ADQUISICIÓN DE BIENES O SERVICIOS SUSTITUTOS;
+// PÉRDIDA DE USO, DATOS O BENEFICIOS; O INTERRUPCIÓN COMERCIAL) SIN EMBARGO Y SOBRE CUALQUIER TEORÍA
+// DE RESPONSABILIDAD, YA SEA POR CONTRATO, RESPONSABILIDAD ESTRICTA O AGRAVIO (INCLUYENDO NEGLIGENCIA
+// O DE OTRA MANERA) QUE SURJA DE CUALQUIER FORMA DEL USO DE ESTE SOFTWARE, AUNQUE SE INDIQUE LA POSIBILIDAD
+// DE DICHO DAÑO.
 //
-// The views and conclusions contained in the software and documentation are those of the
-// authors and should not be interpreted as representing official policies, either expressed
-// or implied, of the University of San Francisco
+// Las opiniones y conclusiones contenidas en el software y la documentación son las de los autores y no
+// deben interpretarse como representaciones de políticas oficiales, ya sea expresas o implícitas,
+// de la Universidad de San Francisco.
+
 
 
 var AnimatedCircle = function(objectID, objectLabel)
